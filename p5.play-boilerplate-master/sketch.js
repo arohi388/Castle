fc827bca6764adb59e983daa31ea9cf3e011e5c9
arohi388 <61878380+arohi388@users.noreply.github.com>
2020-06-14 11:31:51 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(1000,1000);
+
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  rectMode(CENTER)
+  rect(100,1000,100,600)
+  triangle(50,700,150,700,100,500)
+ rect(500,1000,100,600)
+ triangle(450,700,550,700,500,500)
+ rectMode(CENTER)
+ rect(300,1000,300,400)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
